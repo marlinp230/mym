cbd744cb4381c4159d252cb0c7e72d26e20d03bb
Marlyn <marlinp230@gmail.com>
2022-08-12 21:06:22 -0400

    make it better

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ function App() {
       
     }else{
       // https://mym-back.herokuapp.com
-      const res= await axios.post('https://mym-back.herokuapp.com/client',Data)
+      const res= await axios.post('https://mym-back.herokuapp.com/client/',Data)
       console.log(res.data)
      const {message}=res.data;
    
